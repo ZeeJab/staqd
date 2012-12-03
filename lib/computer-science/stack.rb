9483b796eae6bfd3a1ff202805d960c9ac3a4e6c
Dustin Coates <dustin@dcoates.com>
2012-12-03 11:17:48 -0500

    Add stack class

--- lib/computer-science/stack.rb
+++ lib/computer-science/stack.rb
@@ -1,1 +1,17 @@
-stack.rb
+class Stack
+  def initialize
+    @data = []
+  end
+
+  def to_s
+    "There are #{@data.length} items in this stack: #{@data}."
+  end
+
+  def push(value)
+    @data[@data.count] = value
+  end
+
+  def pop
+    @data.delete(@data[-1])
+  end
+end
